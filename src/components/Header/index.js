62eae6244b8d511a2341af0831e524b8c728dcda
celstuv <celstuv@yahoo.fr>
2020-12-16 10:13:05 +0100

    Header ok

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="banner-container">
+      <h1><a href="/"></a>Marvel Quiz</h1>
+      </div>
+    </header>
+  )
+}
+export default Header;
