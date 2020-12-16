fed37f43492605409f1329a80c8fa915d99bb599
celstuv <celstuv@yahoo.fr>
2020-12-16 11:15:34 +0100

    bouton inscription et connexion ok

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Landing = () => {
   return (
     <main className="welcomePage">
-
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
     </main>
   )
 }
