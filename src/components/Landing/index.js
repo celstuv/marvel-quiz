40fa202c500ca926dfa7f3852fb5d19181ea1c95
celstuv <celstuv@yahoo.fr>
2020-12-16 11:48:45 +0100

    Les boutons sur Landing et animation Wolverine

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,31 +1,44 @@
-import React, {useRef, useEffect} from 'react';
-//useRef et useEffect sont des hooks
+import React, { useRef, useEffect, useState, Fragment } from 'react';
+//useRef et useEffect useState, Fragment sont des hooks
 
 const Landing = () => {
+
+  //Création d'une variable d'état à false
+  const [btn, setBtn] = useState(false);
 
   const refWolverine = useRef(null);
   //console.log(refWolverine); pour voir si j'ai une classList
 
-  //Ajout de function pour changer l'image
+  //Ajout de function useEffect pour changer l'image au chargement de la page
   //useEffect s'exécute 1 fois dans return au moment de l'affichage de la page
   useEffect(() => {
     //ajout de css pour obtenir l'image w-men.png
     refWolverine.current.classList.add("startingImg");
-    //retirer limage au bout de 3s
+    //retirer l'image au bout de 1s ainsi que les boutons inscriptions et connexions
     setTimeout(() => {
       refWolverine.current.classList.remove("startingImg");
-    }, 3000);
+      setBtn(true);
+    }, 1000);
   }, [])
 
-  return (
-    //au moment de l'affichage de la page , on a wolverine sans griffe
-    <main ref={refWolverine} className="welcomePage">
+
+  //condition de fonction si Btn est true alors affiche les boutons
+  const displayBtn = btn && (
+    //Fragment = div sans les caractéristiques de la div
+    <Fragment>
       <div className="leftBox">
         <button className="btn-welcome">Inscription</button>
       </div>
       <div className="rightBox">
         <button className="btn-welcome">Connexion</button>
       </div>
+    </Fragment>
+  )
+
+  return (
+    //au moment de l'affichage de la page , on a wolverine sans griffe
+    <main ref={refWolverine} className="welcomePage">
+      { displayBtn }
     </main>
   )
 }
