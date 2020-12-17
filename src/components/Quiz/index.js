7a61bfddb2de902e2aaeb475c710470402006c5d
celstuv <celstuv@yahoo.fr>
2020-12-17 18:56:21 +0100

    Récupération de data depuis cloud Firestore

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Quiz = () => {
-  return (
-      <div>
-      <p>Quiz!</p>
-      </div>
-  )
+class Quiz extends Component {
+
+   render() {
+     const {pseudo} = this.props.userData;
+
+     return (
+       <div>
+         <h2>Pseudo: {pseudo}</h2>
+       </div>
+     )
+   }
 }
+
 export default Quiz;
