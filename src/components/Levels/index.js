80c20aa24b59905678de32a25c90f467afd21a73
celstuv <celstuv@yahoo.fr>
2020-12-21 11:37:06 +0100

    installation de react stepper et tout fonctionne

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Stepper from 'react-stepper-horizontal';
 
 const Levels = () => {
 
   return (
     <div className="levelsContainer">
-      <h2 className="headingLevels">Débutant</h2>
+      <div>
+        <Stepper steps={ [{title: 'Step One'}, {title: 'Step Two'}, {title: 'Step Three'}, {title: 'Step Four'}] } activeStep={ 1 } />
+      </div>
     </div>
   )
 }
